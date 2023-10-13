0b306585c057351b539268bf3f66a1ce957b704e
Erik Huckvale <erik.techcorp@gmail.com>
2023-10-13 15:49:40 -0400

    show map

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Tooltip from "@mui/joy/Tooltip";
+import Box from "@mui/joy/Box";
+import Typography from "@mui/joy/Typography";
 
 function App() {
+  const [clickPosition, setClickPosition] = useState({ x: 0, y: 0 });
+  const [showPopup, setShowPopup] = useState(false);
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const handleClick = (event: React.MouseEvent) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    console.log('#########', x, y)
+    setShowTooltip(false)
+
+    setClickPosition({ x, y });
+    setTimeout(() => {
+      setShowPopup(true);
+    }, 300);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" onClick={handleClick}>
+      {showPopup && (
+        <Tooltip
+          open={showTooltip}
+          placement="top"
+          sx={{
+            boxShadow: "40px 20px 10px #0000006E",
+            border: "2px solid #747474",
+            background: "#191919",
+          }}
+          followCursor
+          enterDelay={100}
+          title={
+            <Box p={1}>
+              <Typography
+                level="h4"
+                textColor="#fff"
+                fontWeight={700}
+                fontSize={18}
+              >
+                Crowbot
+              </Typography>
+              <Box
+                maxWidth={410}
+                color="#BFBFBF"
+                fontSize={14}
+                lineHeight="24px"
+              >
+                Crowbot is an AI model trained on ASU President Dr. Michael
+                Crow’s numerous insights and writings, It provide information
+                and summarizes key perspectives from Dr. Crow on topics related
+                to higher education, innovation, technology and leadership.
+              </Box>
+            </Box>
+          }
+          arrow
         >
-          Learn React
-        </a>
-      </header>
+          <div
+            id="map-selector"
+            onMouseMove={() => setShowTooltip(true)}
+            style={{
+              position: "absolute",
+              width: "180px",
+              height: "90px",
+              top: clickPosition.y - 45,
+              left: clickPosition.x - 90,
+              borderRadius: "50%",
+              border: "3px solid #FFFFFF",
+              background: "#00A3E0 0% 0% no-repeat padding-box",
+              mixBlendMode: "soft-light",
+            }}
+          />
+        </Tooltip>
+      )}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'react-tippy/dist/tippy.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
